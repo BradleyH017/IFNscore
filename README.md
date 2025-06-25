9196f39084076931c6d860d3f7ff1f3f4128a322
BradleyH017 <bh18@sanger.ac.uk>
2025-06-25 12:42:34 +0100

    use python for calculation

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 #### 1. Convert the h5ad object to seurat
 ```
+mkdir -p logs
+input_f="/lustre/scratch127/humgen/projects_v2/sc-eqtl-ibd/analysis/bradley_analysis/IBDverse/atlassing/results/objects/from_irods/celltypist_0.5_ngene_ncount_mt_filt_nomiss.h5ad"
+output_f="input/celltypist_0.5_ngene_ncount_mt_filt_nomiss."
 module load HGI/common/h5ad_to_seurat/0.1
-mkdir -p input
-h5ad_to_seurat /lustre/scratch127/humgen/projects_v2/sc-eqtl-ibd/analysis/bradley_analysis/IBDverse/atlassing/results/objects/from_irods/celltypist_0.5_ngene_ncount_mt_filt_nomiss.h5ad input/celltypist_0.5_ngene_ncount_mt_filt_nomiss.h5ad
+MEM=150000
+bsub -J "conv_seurat" -M"$MEM" -R"select[mem>$MEM] rusage[mem=$MEM] span[hosts=1]" -G team152 \
+    -e logs/conv_seurat-stderr \
+    -o logs/conv_seurat-stdout \
+    "h5ad_to_seurat ${input_f} ${output_f}"
 ```
 
 Also add the module genes to each line of tab delim file. Put this in "input/"
@@ -13,7 +19,7 @@ Also add the module genes to each line of tab delim file. Put this in "input/"
 ```
 mkdir -p logs
 MEM=500000
-bsub -J "get_module_scores-${level}-${hm}" -M"$MEM" -R"select[mem>$MEM] rusage[mem=$MEM] span[hosts=1]" -G team152 \
+bsub -J "get_module_scores" -M"$MEM" -R"select[mem>$MEM] rusage[mem=$MEM] span[hosts=1]" -G team152 \
     -e logs/get_module_scores-stderr \
     -o logs/get_module_scores-stdout \
     "Rscript scripts/estimate_ModuleScores.r 'input/' 'sanger_sample_id' 'Genotyping_ID' 'predicted_labels' 'input/ISG_fixed_all.txt' 'ISG' 'results/'    > \
---
+++ scripts/estimate_ModuleScores.py
@@ -0,0 +1,110 @@
+
+__author__ = 'Bradley Harris'
+__date__ = '2025-06-25'
+__version__ = '0.0.1'
+
+# Change dir
+import os
+cwd = os.getcwd()
+print(cwd)
+
+# Load packages
+import sys
+import os
+sys.path.append('/software/team152/bh18/pip')
+sys.path.append('/usr/local/')
+print("System path")
+print(sys.path)
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import anndata as ad
+import matplotlib as mp
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import rc_context
+
+def parse_options():    
+    # Inherit options
+    parser = argparse.ArgumentParser(
+            description="""
+                Estimating module scores for a specific set gene set
+                """
+        )
+    
+    parser.add_argument(
+            '-i', '--input_file',
+            action='store',
+            dest='input_file',
+            required=True,
+            help=''
+        )
+    
+    parser.add_argument(
+            '-m', '--metadata_cols',
+            action='store',
+            dest='metadata_cols',
+            required=True,
+            help=''
+        )
+
+    parser.add_argument(
+            '-gl', '--gene_listf',
+            action='store',
+            dest='gene_listf',
+            required=True,
+            help=''
+        )
+
+    parser.add_argument(
+            '-gn', '--gene_list_name',
+            action='store',
+            dest='gene_list_name',
+            required=True,
+            help=''
+        )
+
+    parser.add_argument(
+            '-o', '--outname',
+            action='store',
+            dest='outname',
+            required=True,
+            help=''
+        )
+
+    return parser.parse_args()
+
+
+def main():
+    # Parse options
+    inherited_options = parse_options()
+    input_file = inherited_options.input_file # input_file="/lustre/scratch127/humgen/projects_v2/sc-eqtl-ibd/analysis/bradley_analysis/IBDverse/atlassing/results/objects/from_irods/celltypist_0.5_ngene_ncount_mt_filt_nomiss.h5ad"
+    metadata_cols = inherited_options.metadata_cols # metadata_cols = "sanger_sample_id,Genotyping_ID,disease_status,predicted_labels"
+    metadata_cols = metadata_cols.split(",")
+    gene_listf = inherited_options.gene_listf # gene_listf = "input/ISG_fixed_all.txt"
+    gene_list_name = inherited_options.gene_list_name # gene_list_name = "ISG"
+    outname = inherited_options.outname # outname = "results/"
+    
+    # 1. Load
+    print("..Loading object")
+    adata = sc.read_h5ad(input_file)
+    
+    # 2. Load gene set
+    with open(gene_listf) as f:
+        gene_list = [line.strip() for line in f if line.strip()]
+        
+    # 3. Compute module scores using scanpy
+    print("..Computing module scores")
+    sc.tl.score_genes(adata, gene_list, score_name=gene_list_name)
+    
+    # 4. Extract and save
+    print("..Saving")
+    to_save = adata.obs[metadata_cols.append(gene_list_name)]   
+    os.makedirs(outname, exist_ok=True)
+    fullout = f"{outname}{gene_list_name}-module_scores.txt.gz"
+    to_save.to_csv(fullout, sep="\t", index=True, compression='gzip')
+    
+    
+# Execute
+if __name__ == '__main__':
+    main()
+
